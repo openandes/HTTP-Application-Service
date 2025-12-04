9b9833107a3ad391c581043ab91775f41db1bd84
Alabama System Control One <alabama.system@gmail.com>
2025-12-04 16:43:50 -0300

    Integration with GNU Nix Go

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       ## 1. The Package Output (The compiled executable)
       packages = forAllSystems (system: pkgs: {
         default =
-          pkgs.buildGoModule rec { 
+	    gnu-nix-go.lib.buildGo rec { 
             pname = "open-andes-http-application-service";
             version = "0.1.0";
 
